f346ac8ad2dd08c9c622af1a2c02d0d04cf48812
Sean Coombes <seancoombes@live.ca>
2014-11-13 18:12:55 -0500

    highscores now shows some things

--- HelicopterMadness/HelicopterMadness/Scenes/HighScoreScene.cs
+++ HelicopterMadness/HelicopterMadness/Scenes/HighScoreScene.cs
@@ -7,14 +7,16 @@
 
 using System;
 using System.IO;
+using System.Net.Mime;
 using System.Security.Cryptography;
 using System.Threading;
+using System.Xml;
 using HelicopterMadness.Scenes.BaseScene;
 using HelicopterMadness.Scenes.HighScoreComponents;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-using System.IO;
+
 
 namespace HelicopterMadness.Scenes
 {
@@ -22,12 +24,16 @@ namespace HelicopterMadness.Scenes
     public class HighScoreScene : GameScene
     {
         private readonly List<HighScoreEntry> highScoreEntries;
+        private TextDisplay headerDisplay;
+        private TextDisplay scoreDisplay;
 
         private int highestScore;
         private int lowestScore;
 
 
         private const int TOP_SCORES = 5;
+        private const int DUMMY_SCORE = 100;
+        private const string DUMMY_NAME = "ZZZ";
         public HighScoreScene(Game game, SpriteBatch spriteBatch)
             : base(game, spriteBatch)
         {
@@ -36,54 +42,88 @@ namespace HelicopterMadness.Scenes
             string scorepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Highscore.txt");
 
 
-            try
-            {
                 if (File.Exists(scorepath))
                 {
-                    //open file and load into list 
-                    using (StreamReader scores = File.OpenText(scorepath))
+                    try
                     {
-                        int count = 0;
-
-                        while (!scores.EndOfStream && count < TOP_SCORES)
+                        //open file and load into list 
+                        using (StreamReader scores = File.OpenText(scorepath))
                         {
-                            //would this crash if there is no space on the current line?
-                            string[] scoreString = scores.ReadLine().Split(' ');
-                            int score;
+                            int count = 0;
 
-                            //skips line if the score string array doesnt have both parts and if the score is not an int
-                            if (scoreString.Length != 2 || !int.TryParse(scoreString[1], out score))
+                            while (!scores.EndOfStream && count < TOP_SCORES)
                             {
-                                continue;
+
+                                string[] scoreString = scores.ReadLine().Split(' ');
+                                int score;
+
+                                //skips line if the score string array doesnt have both parts and if the score is not an int
+                                if (scoreString.Length != 2 || !int.TryParse(scoreString[1], out score))
+                                {
+                                    continue;
+                                }
+
+                                //edits the name so it fits the decided on patter of 3 chars in uppcase with no spaces
+                                scoreString[0] = scoreString[0].Trim().PadRight(3).Replace(' ', 'A').ToUpper();
+
+                                //highScoreEntries[count] = new HighScoreEntry(scoreString[0], score);
+                                highScoreEntries.Add(new HighScoreEntry(scoreString[0], score));
+                                count++;
                             }
-
-                            //edits the name so it fits the decided on patter of 3 chars in uppcase with no spaces
-                            scoreString[0] = scoreString[0].Trim().PadRight(3).Replace(' ', 'A').ToUpper();
-
-                            highScoreEntries[count] = new HighScoreEntry(scoreString[0], score);
-                            count++;
-
-                            
                         }
+
+                        //add dummmy scores to the list if any scores from the above while loop were invalid
+                        while (highScoreEntries.Count < 5 && highScoreEntries.Count > 0)
+                        {
+                            //highScoreEntries[highScoreEntries.Count] = new HighScoreEntry(DUMMY_NAME, 0);
+                            highScoreEntries.Add(new HighScoreEntry(DUMMY_NAME,0));
+                        }
+
+                        if(highScoreEntries.Count == 0)
+                        {
+                            prepDummyList();
+                        }
+
+                        highestScore = highScoreEntries[0].Score;
+                        lowestScore = highScoreEntries[TOP_SCORES - 1].Score;
+                        
+                    }
+                    catch (Exception)
+                    {
+                        
+                        throw;
                     }
                 }
                 else
                 {
-                    //create list of dummy data
-                    for (int i = 0; i < TOP_SCORES; i++)
-                    {
-
-                        //highScoreEntries[i] = new HighScoreEntry(scoreString[0], score);
-                    }
+                    prepDummyList();
                 }
-            }
-            catch (Exception ex)
+
+            //TEMP Testing text alignments and what not
+            SpriteFont headerFont = game.Content.Load<SpriteFont>("Fonts/Regular");
+            Vector2 scoreDim = headerFont.MeasureString("HIGHSCORES\n\n\n");
+            Vector2 scorePos = new Vector2(SharedSettings.Stage.X/2 - scoreDim.X/2, 0);
+            headerDisplay = new TextDisplay(game,spriteBatch,headerFont,scorePos,Color.Red);
+            headerDisplay.Message = "HIGHSCORES";
+
+            Components.Add(headerDisplay);
+        }
+
+        private void prepDummyList()
+        {
+            
+            //create list of dummy data
+            for (int i = 0; i < TOP_SCORES; i++)
             {
-
+                int score = DUMMY_SCORE;
+                score = score - i * 10;
+                //you tricked my Drew this dont work
+                //highScoreEntries[i] = new HighScoreEntry(DUMMY_NAME, score);
+                highScoreEntries.Add(new HighScoreEntry(DUMMY_NAME, score));
             }
-            
-            
 
+            highestScore = highScoreEntries[0].Score;
+            lowestScore = highScoreEntries[TOP_SCORES - 1].Score;
         }
 
         public int HighestScore
@@ -96,13 +136,15 @@ namespace HelicopterMadness.Scenes
             get { return lowestScore; }
         }
 
+
+
         /// <summary>
         /// Allows the game component to update itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            // TODO: Remove this or implement updates
+            // TODO: Remove this or impBlement updates
 
             base.Update(gameTime);
         }
